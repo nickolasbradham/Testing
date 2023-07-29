1eca3562307ed11eef59a4fbf4ac14e34967e6fb
nickolasbradham <nickolasbradham@gmail.com>
2023-07-29 08:41:08 -0600

    Restart

--- src/nbradham/testing/Recipe.java
+++ src/nbradham/testing/Recipe.java
@@ -1,46 +1,6 @@
 package nbradham.testing;
 
-final class Recipe {
-	private final String name, machine;
-	private final ItemStack[] input, output;
-	private final short time;
-	float weight = Float.POSITIVE_INFINITY;
+import java.util.HashMap;
 
-	Recipe(String rName, String rMachine, ItemStack[] rInput, short rTime, ItemStack[] rOutput) {
-		name = rName;
-		machine = rMachine;
-		input = rInput;
-		output = rOutput;
-		time = rTime;
-	}
-
-	void setWeight(float newWeight) {
-		System.out.printf("Weight (%s) %f -> %f%n", name, weight, newWeight);
-		weight = newWeight;
-	}
-
-	ItemStack[] getInput() {
-		return input;
-	}
-
-	ItemStack[] getOutput() {
-		return output;
-	}
-
-	String getName() {
-		return name;
-	}
-
-	short getTime() {
-		return time;
-	}
-
-	float getWeight() {
-		return weight;
-	}
-
-	@Override
-	public final String toString() {
-		return weight + " " + name;
-	}
+record Recipe(String name, String machine, HashMap<String, Float> inputs, short time, HashMap<String, Float> outputs) {
 }
--- src/nbradham/testing/Tester.java
+++ src/nbradham/testing/Tester.java
@@ -1,66 +1,37 @@
 package nbradham.testing;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 final class Tester {
 
-	private final HashMap<String, ProdDetails> prodDetails = new HashMap<>();
-	private final Queue<Recipe> queue = new LinkedList<>();
+	private static final String DELIM = "\t|\r\n";
+
+	private final HashMap<String, Item> items = new HashMap<>();
 
 	private void start() {
-		Scanner s = new Scanner(Tester.class.getResourceAsStream("/raws.tsv")).useDelimiter("\t|\n");
-		s.nextLine();
-		while (s.hasNext()) {
-			prodDetails.put(s.next(), new ProdDetails(s.nextFloat()));
-			s.nextLine();
+		Scanner scan = new Scanner(Tester.class.getResourceAsStream("/raws.tsv")).useDelimiter(DELIM);
+		scan.nextLine();
+		while (scan.hasNext()) {
+			items.put(scan.next(), new Item(scan.nextFloat()));
+			scan.nextLine();
 		}
-		s.close();
-
-		s = new Scanner(Tester.class.getResourceAsStream("/recipes.tsv")).useDelimiter("\t|\n");
-		s.nextLine();
-		Recipe r;
-		while (s.hasNext()) {
-			queue.add(r = new Recipe(s.next(), s.next(), ItemStack.parseStacks(s.next()), s.nextShort(),
-					ItemStack.parseStacks(s.next().strip())));
-			for (ItemStack is : r.getInput()) {
-				prodDetails.putIfAbsent(is.name(), new ProdDetails(Float.POSITIVE_INFINITY));
-				prodDetails.get(is.name()).addConsumer(r);
-			}
-			for (ItemStack is : r.getOutput()) {
-				prodDetails.putIfAbsent(is.name(), new ProdDetails(Float.POSITIVE_INFINITY));
-				prodDetails.get(is.name()).addProducer(r);
-			}
+		System.out.println(items);
+		scan.close();
+		scan = new Scanner(Tester.class.getResourceAsStream("/recipes.tsv")).useDelimiter(DELIM);
+		scan.nextLine();
+		while (scan.hasNext()) {
+			Recipe recipe = new Recipe(scan.next(), scan.next(), Item.parseItemStacks(scan.next()), scan.nextShort(),
+					Item.parseItemStacks(scan.next()));
+			recipe.outputs().keySet().forEach(i -> {
+				items.putIfAbsent(i, new Item());
+				items.get(i).addRecipe(recipe);
+			});
 		}
-
-		float sum, rate;
-		ArrayList<Recipe> cs = new ArrayList<>();
-		while (!queue.isEmpty()) {
-			sum = 0;
-			rate = 60 / (r = queue.poll()).getTime();
-			System.out.printf("Processing %s... ", r.getName());
-			for (ItemStack is : r.getInput())
-				sum += rate * prodDetails.get(is.name()).getWeight();
-			System.out.println(sum);
-			if (sum < r.getWeight()) {
-				r.setWeight(sum);
-				cs.clear();
-				for (ItemStack is : r.getOutput())
-					cs.addAll(Arrays.asList(prodDetails.get(is.name()).getConsumers()));
-				queue.addAll(cs);
-				System.out.printf("ADDED MORE: %s%n", cs);
-			}
-		}
-		prodDetails.values().forEach(v -> v.update());
-		prodDetails.forEach((k, v) -> System.out.printf("%s -> %s%n", k, Arrays.toString(v.getProducers())));
+		System.out.println(items);
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Started.");
 		new Tester().start();
 	}
 }
