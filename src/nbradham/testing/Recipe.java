43ceda296e586b79db72ecd5a869b2922ac98f41
nickolasbradham <nickolasbradham@gmail.com>
2023-08-04 22:52:06 -0600

    nope

--- src/nbradham/testing/Recipe.java
+++
@@ -1,6 +0,0 @@
-package nbradham.testing;
-
-import java.util.HashMap;
-
-record Recipe(String name, String machine, HashMap<String, Float> inputs, short time, HashMap<String, Float> outputs) {
-}
--- src/nbradham/testing/Tester.java
+++ src/nbradham/testing/Tester.java
@@ -1,37 +1,6 @@
 package nbradham.testing;
 
-import java.util.HashMap;
-import java.util.Scanner;
-
 final class Tester {
-
-	private static final String DELIM = "\t|\r\n";
-
-	private final HashMap<String, Item> items = new HashMap<>();
-
-	private void start() {
-		Scanner scan = new Scanner(Tester.class.getResourceAsStream("/raws.tsv")).useDelimiter(DELIM);
-		scan.nextLine();
-		while (scan.hasNext()) {
-			items.put(scan.next(), new Item(scan.nextFloat()));
-			scan.nextLine();
-		}
-		System.out.println(items);
-		scan.close();
-		scan = new Scanner(Tester.class.getResourceAsStream("/recipes.tsv")).useDelimiter(DELIM);
-		scan.nextLine();
-		while (scan.hasNext()) {
-			Recipe recipe = new Recipe(scan.next(), scan.next(), Item.parseItemStacks(scan.next()), scan.nextShort(),
-					Item.parseItemStacks(scan.next()));
-			recipe.outputs().keySet().forEach(i -> {
-				items.putIfAbsent(i, new Item());
-				items.get(i).addRecipe(recipe);
-			});
-		}
-		System.out.println(items);
-	}
-
 	public static void main(String[] args) {
-		new Tester().start();
 	}
 }
