2921edf01faa07f3d8b04178bffe59ee64315eb6
nickolasbradham <nickolasbradham@gmail.com>
2023-11-22 15:57:57 -0700

    work

--- src/nbradham/testing/Tester.java
+++ src/nbradham/testing/Tester.java
@@ -1,28 +1,23 @@
 package nbradham.testing;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 
 final class Tester {
 
 	public static void main(String[] args) throws IOException {
-		Process p = Runtime.getRuntime()
-				.exec(new String[] { "C:\\Program Files (x86)\\Steam\\steamapps\\common\\U3DS\\Unturned.exe" });
-		Scanner s = new Scanner(p.getInputStream());
-		PrintWriter pw = new PrintWriter(p.getOutputStream(), true);
-		Scanner sSTD = new Scanner(System.in);
-		new Thread(() -> {
-			while (s.hasNext())
-				System.out.printf("[CHILD] %s%n", s.nextLine());
-			System.out.println("Thread ended.");
-		}).start();
-		String st;
-		while (p.isAlive()) {
-			st = sSTD.nextLine();
-			System.out.printf("[HOST] Sending to child: %s%n", st);
-			pw.println(st);
+		StringBuilder token;
+		boolean run = true;
+		int c;
+		while (run) {
+			token = new StringBuilder();
+			while ((c = System.in.read()) != '\r') {
+				switch (c) {
+				default:
+					token.append((char) c);
+				}
+			}
+			System.out.println(token);
+			run = !token.toString().equals("exit");
 		}
-		sSTD.close();
 	}
 }
